36de448759105db25800cfc7825f7fcc6f2a2550
raizo0426 <50013256025@st.tuat.ac.jp>
2015-10-10 09:00:37 +0000

    pre- create messages-controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  get 'messages/index'
+
+  root 'messages#index'
+  resources :message , only: [:create]
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
